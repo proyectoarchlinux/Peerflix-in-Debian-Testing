e11d46c9e6d4437bd577bd109af0b8da57bfbe7a
Daniel Nogales Iglesias <44336888+proyectoarchlinux@users.noreply.github.com>
2019-01-23 17:31:21 +0100

    Add files via upload

---
+++ install-peerflix.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+INSTALL1="sudo apt-get install nodejs npm"
+INSTALLNPM="sudo npm install -g peerflix"
+
+echo instalando...
+sleep 1 && echo 3
+sleep 1 && echo 2
+sleep 1 && echo 1
+sleep 1
+
+$INSTALL1 && $INSTALLNPM
